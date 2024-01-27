2fafa5f56daa031df01d1809fc659af87e9280b7
ridhoalba <e32210496@student.polije.ac.id>
2024-01-27 19:43:51 +0700

    add step in groovy

--- src/programmerzamannow/jenkins/Output.groovy
+++ src/programmerzamannow/jenkins/Output.groovy
@@ -2,7 +2,7 @@ package programmerzamannow.jenkins;
 
 class Output {
 
-    static def hello(String name) {
-        println("Hello ${name}")
+    static def hello(steps, String name) {
+        steps.echo("Hello ${name}")
     }
 }
